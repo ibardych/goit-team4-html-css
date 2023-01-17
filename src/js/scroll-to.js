e19d00ee5bbd4bbdc02600e77d0c835b5baa3f77
yanachepyk <yanapgp@gmail.com>
2023-01-18 01:14:32 +0200

    show header bottom border on scrol

--- src/js/scroll-to.js
+++ src/js/scroll-to.js
@@ -1,5 +1,6 @@
 (() => {
     const navLinks = document.querySelectorAll('[data-nav-link]');
+    const header = document.getElementsByTagName('header')[0];
 
     function scrollToSection(element) {
         const elementToNavigate = document.querySelector(`#${element.target.dataset.navLink}`);
@@ -8,4 +9,12 @@
     } 
 
     navLinks.forEach((navLink) => navLink.addEventListener('click', scrollToSection));
+
+    window.onscroll = (event) => {
+        if (window.scrollY > 0) {
+            header.classList?.add('header--border');
+        } else {
+            header.classList?.remove('header--border');
+        }
+    }
 })();
